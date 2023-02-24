44cf6abdc6d8e9320f5bea3d3d17db06b0bb1249
Marcysp <marcyalvina@gmail.com>
2023-02-24 21:51:17 +0700

    coba commit ulang

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\WelcomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,50 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// akan memanggil
+// 127.0.0.1:8000/ ==> view welcome
 Route::get('/', function () {
     return view('welcome');
 });
+
+// // 127.0.0.1:8000/hello ==> view Hello world
+Route::get('/hello', function () {
+    return 'Hello world';
+});
+
+// // 127.0.0.1:8000/hello/id ==> view Hello (id) wlcome to this web
+Route::get('/hello/{id}', function ($id) {
+    return "<h3> HELLO $id WELCOME TO THIS WEB";
+});
+
+// Route::get('mahasiswa', function ($id) {
+// });
+
+// Route::post('mahasiswa', function ($id) {
+// });
+
+// Route::put('mahasiswa', function ($id) {
+// });
+
+// Route::delete('mahasiswa', function ($id) {
+// });
+
+// Route::get('mahasiswa/{id}', function ($id) {
+// });
+
+// Route::post('mahasiswa/{id}', function ($id) {
+// });
+
+// Route::put('mahasiswa/{id}', function ($id) {
+// });
+
+// Route::delete('mahasiswa/{id}', function ($id) {
+// });
+
+// Route::match(['get', 'post'], '/specialUrl', function () {
+// });
+
+// Route::any('/specialMahasiswa', function ($id) {
+// });
+
+Route::get('siswa', [WelcomeController::class, 'hello']);
